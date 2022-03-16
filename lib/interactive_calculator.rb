970becdbd16bc4b7cd49eea3ac431f5f290d0a37
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-16 15:52:00 +0000

    Completed exercise

--- lib/interactive_calculator.rb
+++ lib/interactive_calculator.rb
@@ -1,13 +1,25 @@
 class InteractiveCalculator
+  def initialize(terminal)
+    @terminal = terminal
+    @first_num = ""
+    @sec_num = ""
+    @sub_res = 0
+  end
+
   def input
-    # gets user input from terminal
+    @terminal.puts "Hello. I will subtract two numbers.\nPlease enter a number\n"
+    @first_num = @terminal.gets.chomp
+    @terminal.puts "Please enter another number\n"
+    @sec_num = @terminal.gets.chomp
   end
 
   def subtract
+    @sub_res = @first_num.to_i - @sec_num.to_i
     # subtracts the second user input from the first
   end
   
   def result
+    @terminal.puts"Here is your result:\n#{@first_num} - #{@sec_num} = #{@sub_res}"
     # puts the formatted subtraction output to terminal
   end
 end
@@ -16,8 +28,8 @@ end
 
 
 
-interactive_calculator = InteractiveCalculator.new(Kernel)
-interactive_calculator.run
+#interactive_calculator = InteractiveCalculator.new(Kernel)
+#interactive_calculator.run
 
 # Hello. I will subtract two numbers.
 # Please enter a number
--- spec/interactive_calculator_spec.rb
+++ spec/interactive_calculator_spec.rb
@@ -2,11 +2,36 @@ require 'interactive_calculator'
 
 RSpec.describe InteractiveCalculator do
   it "gets user input from the terminal" do
+    terminal = double :terminal
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.\nPlease enter a number\n").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number\n").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    new_calculator = InteractiveCalculator.new(terminal)
+    new_calculator.input
   end
 
   it "subtracts the second user input from the first" do
+    terminal = double :terminal
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.\nPlease enter a number\n").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number\n").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    new_calculator = InteractiveCalculator.new(terminal)
+    new_calculator.input
+    expect(new_calculator.subtract).to eq 1
   end
 
   it "puts the formatted subtraction output to terminal" do
+    terminal = double :terminal
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.\nPlease enter a number\n").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number\n").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:\n4 - 3 = 1")
+    new_calculator = InteractiveCalculator.new(terminal)
+    new_calculator.input
+    new_calculator.subtract
+    new_calculator.result
   end
 end
