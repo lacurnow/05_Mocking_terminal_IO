3922543a69f73d5a6cab57d8f94a0da13d7fa3d8
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-16 15:17:03 +0000

    Project initialised

---
+++ lib/interactive_calculator.rb
@@ -0,0 +1,28 @@
+class InteractiveCalculator
+  def input
+    # gets user input from terminal
+  end
+
+  def subtract
+    # subtracts the second user input from the first
+  end
+  
+  def result
+    # puts the formatted subtraction output to terminal
+  end
+end
+
+
+
+
+
+interactive_calculator = InteractiveCalculator.new(Kernel)
+interactive_calculator.run
+
+# Hello. I will subtract two numbers.
+# Please enter a number
+# 4
+# Please enter another number
+# 3
+# Here is your result:
+# 4 - 3 = 1
---
+++ spec/interactive_calculator_spec.rb
@@ -0,0 +1,12 @@
+require 'interactive_calculator'
+
+RSpec.describe InteractiveCalculator do
+  it "gets user input from the terminal" do
+  end
+
+  it "subtracts the second user input from the first" do
+  end
+
+  it "puts the formatted subtraction output to terminal" do
+  end
+end
